37576ccbf3be2cd93e973eb5bf477c1ee0869dde
Halim Ceylan <halimceylan7@gmail.com>
2025-01-04 19:24:24 +0100

    problem 18 solved and problem 19 possible solution

---
+++ problem19.py
@@ -0,0 +1,3 @@
+# TO DO 
+# WORST SOLUTION FOR THE PROBLEM 
+# find the which day is first day of the 1 jan on 1901 then calulcate how many day pass until 31 Dec 2000 divide into 7 and calculate the exact day by using remains
